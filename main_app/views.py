807c74b2f56ced41252a9f6bcc2fd654fb03e631
Kyle Canamar <canamar.kyle@gmail.com>
2022-07-19 23:18:05 -0700

    implements authentication, creates base.html & templates directory & adds models to admin registry

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+import uuid
+import boto3
+
+S3_BASE_URL = ''
+BUCKET = ''
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = "Invaild sign up - try again"
+    form = UserCreationForm()
+    context = {'form': form, "error_message": error_message}
+    return render(request, 'registration/signup.html', context)
