dc5ed7d39f0e72b87da8f4e12f8545d891d80281
Sunghun Park <sunghun.josephp@gmail.com>
2022-07-19 22:11:23 -0700

    Add Product and Photo class in models.py

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Product(models.Model):
+	name = models.CharField(max_length=100)
+	price = models.IntegerField()
+	description = models.TextField(max_length=300)
+
+	def __str__(self):
+		return f'Product {self.name} with a price of {self.price}'
+	
+	def get_absolute_url(self):
+		return reverse('detail', kwargs = {'product_id': self.id})
+
+class Photo(models.Model):
+	url = models.CharField(max_length=200)
+	product = models.ForeignKey(Product, on_delete=models.CASCADE)
+	def __str__(self):
+		return f'Photo for product_id: {self.product_id} @{self.url}'
